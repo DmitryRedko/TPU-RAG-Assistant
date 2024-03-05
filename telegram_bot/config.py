481d40bd0cf98fac8207d09480c660e7aa71266e
Dmitry <dimaredko444@gmail.com>
2024-03-06 00:56:37 +0700

    update

--- telegram_bot/config.py
+++
@@ -1,3 +0,0 @@
-OPEN_AI_TOKEN = "sk-e3e90c3e79774a82865cb7b2f06d3fee"
-WEB_URL = "https://api.deepseek.com/v1"
-BOT_TOKEN = "7144663060:AAH1j4VcU1uuQh5tW58feIKfHUaGXT4GI5M"
--- telegram_bot/tpu_model.py
+++ telegram_bot/tpu_model.py
@@ -1,4 +1,3 @@
-from tpu_info_base import TextSearchEngine
 from openai import OpenAI
 from config import OPEN_AI_TOKEN, WEB_URL
 
@@ -13,7 +12,7 @@ class DialogAssistant:
             model="deepseek-chat",
             messages=[
                 {"role": "system", 
-                 "content": f"""Ты ассистент помощник студентам Томского Политехнического Университета. Тебя создали программисты ТПУ. Тебя зовут политехник! Отвечай так на всех языках! Ты не Алиса и не DeepSeek! Ответ давай не более чем 200 слов. Ответ дай на основе базы знаний: {info_base}. Пользуйся историей диалога {history} Ссылки ищи в базе."""},
+                 "content": f"""Ты ассистент помощник студентам Томского Политехнического Университета. Тебя создали программисты ТПУ. Тебя зовут Политехник! Отвечай так на всех языках! Ты не Алиса и не DeepSeek! Ответ давай не более чем 200 слов. Ответ дай на основе базы знаний: {info_base}. Пользуйся историей диалога {history} Ссылки ищи в базе."""},
                 {"role": "user", "content": user_question},
         ],
             max_tokens=512,
@@ -23,17 +22,6 @@ class DialogAssistant:
         answer = response.choices[0].message.content
         self.save_history(answer)
         return answer
-    
-    def get_base_info(self, user_question):
-        info_base = self.search_engine.get_closest_matching_text(user_question)
-        res_str = ""
-        break_counter=0
-        for i in range(len(info_base)):
-            res_str += str([info_base[i]])
-            break_counter+=1
-            if(break_counter==3):
-                break
-        return res_str
     
     def get_history(self):
         return self.history
